a4c9fad2a9a90bd9921b4a387763d1a69de74467
Darnell Garcia <darnellgarcia26@gmail.com>
2026-01-11 19:41:49 -0400

    Updated Work Section

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -3,13 +3,18 @@ import config from "../index.json";
 
 const Header = () => {
   const navigation = config.navigation;
+
   return (
     <div className="header">
       <div className="header__menu">
-        <ul className="flex px-8 lg:px-32 gap-x-10 content-center leading-0 h-0">
+        <ul className="flex flex-col lg:flex-row px-4 lg:px-32 gap-y-4 lg:gap-x-10 items-center">
           {navigation.map((item) => (
-            <li className="mt-6 cursor-pointer" key={item.title}>
-              <a href={`${item.url}`} target={item.title == "My Blog" ? "_blank" : undefined}>
+            <li className="mt-4 lg:mt-6 cursor-pointer" key={item.title}>
+              <a
+                href={item.url}
+                target={item.title === "My Blog" ? "_blank" : undefined}
+                rel={item.title === "My Blog" ? "noopener noreferrer" : undefined}
+              >
                 {item.title}
               </a>
             </li>
