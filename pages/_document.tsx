58f667f90b23725a9e535ab775642d2768c3ad14
Darnell Garcia <darnellgarcia26@gmail.com>
2026-01-16 00:13:07 -0400

    Privacy Page Fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
 
 export default function Document() {
   return (
-    <Html lang="en" className='scroll-smooth'>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
-        <title>Darnell Garcia - Portfolio</title>
-        <meta
-          name="description"
-          content="Welcome to my personal portfolio website"
-          key="desc"
-        />
-        <meta property="og:title" content="Darnell Garcia - Portfolio" />
-        <meta property="og:description" content="Welcome to my personal portfolio website" />
-        <meta property="og:image" content="ZeroDayz77/My-Portfolio-Templeted-Built/public/favicon.ico" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://darnellgarcia.vercel.app/" />
+    <Html lang="en" className="scroll-smooth">
       <Head />
-      <body className='text-black'>
+      <body className="text-black">
+        <Main />
         <NextScript />
-        <Header />
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
-        <Footer />
       </body>
     </Html>
   )
