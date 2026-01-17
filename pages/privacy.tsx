7aa8cd784961c91eda6378198e85775281e64115
Darnell Garcia <darnellgarcia26@gmail.com>
2026-01-16 21:06:04 -0400

    Fixed Location Of Privacy Title

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 export default function PrivacyPage() {
   return (
         <>
+            <title>Privacy Policy - Production Zero</title>
             <Header />
             <Privacy />
         </>
